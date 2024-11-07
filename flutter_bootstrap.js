7cbe328c6ff4f78f09335787641a5e0d364db0b2
Maksym <m.levytskiy@gmail.com>
2024-11-07 17:24:23 +0200

    update web release files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "25459655"
+    serviceWorkerVersion: "3830014349"
   }
 });
